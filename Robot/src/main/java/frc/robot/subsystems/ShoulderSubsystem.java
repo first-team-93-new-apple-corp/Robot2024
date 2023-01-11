068afccac5d299b4273b6095c4c73a4d00b7590c
da_winkz <neonitrogen127@gmail.com>
2023-01-11 11:57:24 -0600

    Added Shoulder and Telescope Subsystems

---
+++ Robot/src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShoulderSubsystem extends SubsystemBase {
+
+
+
+  enum ArmState {
+    DEFAULT_STATE,
+    GROUND_LOAD,
+    PLAYER_LOAD,
+    MID_CONE,
+    MID_CUBE,
+    HIGH_CUBE,
+    HIGH_CONE,
+    LOW_HYBRID
+  }
+
+  public CANSparkMax ShoulderMotor1; 
+  public CANSparkMax ShoulderMotor2;
+  RelativeEncoder ShoulderMotor1Encoder;
+  RelativeEncoder ShoulderMotor2Encoder;
+
+  SparkMaxPIDController ShoulderPIDController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+
+  public ShoulderSubsystem() {
+  ShoulderMotor1 = new CANSparkMax(0, MotorType.kBrushless);
+  ShoulderMotor2 = new CANSparkMax(0, MotorType.kBrushless);
+  ShoulderMotor1Encoder = ShoulderMotor1.getEncoder();
+  ShoulderMotor2Encoder = ShoulderMotor2.getEncoder();
+  ShoulderPIDController = ShoulderMotor1.getPIDController();
+
+  kP = 0.1; 
+  kI = 1e-4;
+  kD = 1; 
+  kIz = 0; 
+  kFF = 0; 
+  kMaxOutput = 1; 
+  kMinOutput = -1;
+
+  // set PID coefficients
+  ShoulderPIDController.setP(kP);
+  ShoulderPIDController.setI(kI);
+  ShoulderPIDController.setD(kD);
+  ShoulderPIDController.setIZone(kIz);
+  ShoulderPIDController.setFF(kFF);
+  ShoulderPIDController.setOutputRange(kMinOutput, kMaxOutput);
+
+  }
+  public void RunShoulderMotors(){
+  double rotations = 0; //TODO
+  ShoulderPIDController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+  }
+
+ 
+
+  @Override public void periodic() {
+
+  }
+
+  @Override
+  public void simulationPeriodic() {}
+}
---
+++ Robot/src/main/java/frc/robot/subsystems/TelescopingSubsystem.java
@@ -0,0 +1,62 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TelescopingSubsystem extends SubsystemBase {
+
+  enum TelescopeState {
+    DEFAULT_STATE,
+    GROUND_LOAD,
+    PLAYER_LOAD,
+    MID_CONE,
+    MID_CUBE,
+    HIGH_CUBE,
+    HIGH_CONE,
+    LOW_HYBRID
+  }
+
+  public CANSparkMax TelescopingMotor1; 
+  RelativeEncoder TelescopingMotor1Encoder;
+
+  SparkMaxPIDController TelescopingPIDController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+
+  public TelescopingSubsystem() {
+  TelescopingMotor1 = new CANSparkMax(0, MotorType.kBrushless);
+  TelescopingMotor1Encoder= TelescopingMotor1.getEncoder();
+    TelescopingPIDController = TelescopingMotor1.getPIDController();
+  kP = 0.1; 
+  kI = 1e-4;
+  kD = 1; 
+  kIz = 0; 
+  kFF = 0; 
+  kMaxOutput = 1; 
+  kMinOutput = -1;
+
+  // set PID coefficients
+  TelescopingPIDController.setP(kP);
+  TelescopingPIDController.setI(kI);
+  TelescopingPIDController.setD(kD);
+  TelescopingPIDController.setIZone(kIz);
+  TelescopingPIDController.setFF(kFF);
+  TelescopingPIDController.setOutputRange(kMinOutput, kMaxOutput);
+
+  }
+  public void RunTelescopingMotors(){
+  double rotations = 0; //TODO
+  TelescopingPIDController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+  }
+
+ 
+
+  @Override public void periodic() {
+
+  }
+
+  @Override
+  public void simulationPeriodic() {}
+}
