889d5be76c3eb2de84de57ff0e214c6b94c9c75e
jsmmr2 <justintsommer@icloud.com>
2023-09-07 10:03:00 -0500

    [huh[ what is astar

--- Robot/src/main/java/frc/robot/AStar/Nodes.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.AStar;
-
-public class Nodes {
-    
-}
--- Robot/src/main/java/frc/robot/AStar/main.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.AStar;
-
-public class main {
-    public static void main(String[] args) {
-        
-    }
-}
