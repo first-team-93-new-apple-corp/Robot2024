708f6e74d06ad046a5f07d22686b962543c78081
LavaWolf911 <Connorw911@gmail.com>
2023-12-05 20:04:14 -0600

    Large Prototype!

--- Robot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -80,32 +80,6 @@ public class ElevatorSubsystem extends SubsystemBase {
     } else {
       ElevMotor.set((ElevJoystick.getY()) * (ElevJoystick.getY()));
     }
-    
-  }
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public CommandBase exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a
-   * digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
   }
 
   @Override
