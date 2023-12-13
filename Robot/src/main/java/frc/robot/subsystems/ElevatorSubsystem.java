fe81d586e46024ad17ac515a6912802834ee081e
LavaWolf911 <Connorw911@gmail.com>
2023-12-12 20:17:15 -0600

    Co-authored-by: jotishk <jotishk@users.noreply.github.com>

--- Robot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -24,8 +24,7 @@ public class ElevatorSubsystem extends SubsystemBase {
   int gearRatio = 25; // Change later!??!?!?
   boolean driverControl = false;
   boolean run = true;
-  WPI_TalonFX ElevMotorLeft = new WPI_TalonFX(01);
-  WPI_TalonFX ElevMotorRight = new WPI_TalonFX(01);
+  WPI_TalonFX ElevMotor = new WPI_TalonFX(01);
   Joystick ElevJoystick = new Joystick(0);
   ElevatorFeedforward ElevFeedforward = new ElevatorFeedforward(maxHeight, 0.21, 24.47, 0.03);
 
@@ -67,20 +66,17 @@ public class ElevatorSubsystem extends SubsystemBase {
   }
 
   public void goToSetpoint() {
-    ElevMotorRight.set(ElevFeedforward.calculate(elevatorPID.calculate(ElevMotorRight.getSelectedSensorPosition(), currentsetpoint)));
-    ElevMotorLeft.set(ElevFeedforward.calculate(elevatorPID.calculate(ElevMotorLeft.getSelectedSensorPosition(), -currentsetpoint)));
+    ElevMotor.set(ElevFeedforward.calculate(elevatorPID.calculate(ElevMotor.getSelectedSensorPosition(), currentsetpoint)));
   }
 
   public void JoystickControl() {
-    if (((ElevMotorLeft.getSelectedSensorPosition() > maxHeight) && (ElevJoystick.getY() > 0))
-        || ((ElevMotorLeft.getSelectedSensorPosition() < minHeight) && (ElevJoystick.getY() < 0))) {
+    if (((ElevMotor.getSelectedSensorPosition() > maxHeight) && (ElevJoystick.getY() > 0))
+        || ((ElevMotor.getSelectedSensorPosition() < minHeight) && (ElevJoystick.getY() < 0))) {
       return;
     } else if (limitSwitch.get() && ElevJoystick.getY() < 0) {
-      ElevMotorRight.set(0);
-      ElevMotorLeft.set(0);
+      ElevMotor.set(0);
     } else {
-      ElevMotorRight.set((ElevJoystick.getY()) * (ElevJoystick.getY()));
-      ElevMotorLeft.set((ElevJoystick.getY()) * (-ElevJoystick.getY()));
+      ElevMotor.set((ElevJoystick.getY()) * (ElevJoystick.getY()));
     }
   }
 
