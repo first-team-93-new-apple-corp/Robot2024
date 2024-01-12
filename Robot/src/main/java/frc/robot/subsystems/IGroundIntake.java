a9d824065db1aa689ed519af3172a14b76191735
FInnley Coutler <COU544476@stu.aasd.k12.wi.us.Finnley>
2024-01-11 18:49:55 -0800

    Started making interfaces for the subsystems

---
+++ Robot/src/main/java/frc/robot/subsystems/IGroundIntake.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems;
+
+public interface IGroundIntake {
+    public void intakeStart();
+    public void intakeStop();
+}
---
+++ Robot/src/main/java/frc/robot/subsystems/IShooter.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems;
+
+public interface IShooter {
+    public void intake();
+    public void stop();
+    public void shootAmp();
+    public void shootSpeaker();
+}
