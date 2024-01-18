2245abb627509281ce1eb4508c75a3694bfbc56d
da_winkz <neonitrogen127@gmail.com>
2024-01-18 17:26:33 -0600

    cleaned up robot24 main

--- Robot-Imported/src/main/java/frc/robot/subsystems/IShooter.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems;
-
-public interface IShooter extends ISubsystem{
-    public void intake();
-    public void stop();
-    public void shootAmp();
-    public void shootSpeaker();
-}
--- Robot/src/main/java/frc/robot/subsystems/Interfaces/IGroundIntake.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.Interfaces;
-
-public interface IGroundIntake extends ISubsystem{
-    public void intakeStart();
-    public void intakeStop();
-    public void intakeRunover();
-}
