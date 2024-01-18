602c0e3375722250c71f7800642fb8c5f5cf8577
Driver 2 <som624686@stu.aasd.k12.wi.us>
2024-01-18 13:37:12 -0600

    [Working] Created a Tuner X Swerve project

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,98 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.AutonSubsystem;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.OperatorInterfaceSubsystem;
-import frc.robot.subsystems.VisionSubsystem;
-// import frc.robot.commands.AutonCommands.DriveAndLevel;
-import frc.robot.commands.AutonCommands.LockWheels;
-import frc.robot.commands.HumanDrive;
-import frc.robot.commands.ShooterAndIntakeCmd;
+import frc.robot.generated.TunerConstants;
 
 public class RobotContainer {
-  // Subsystems
-  OperatorInterfaceSubsystem m_OperatorInterfaceSubsystem;
-  DriveSubsystem m_DriveSubsystem;
-  AutonSubsystem m_AutonSubsystem;
-  VisionSubsystem m_VisionSubsystem;
-  Joystick Driver2;
+  private double MaxSpeed = 6; // 6 meters per second desired top speed
+  private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  // private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
+  private final Joystick m_Joystick1 = new Joystick(0);
+  private final Joystick m_Joystick2 = new Joystick(1);
+  private final JoystickButton m_JoystickTrigger = new JoystickButton(m_Joystick1, 1);
+  private final JoystickButton m_JoystickButton2 = new JoystickButton(m_Joystick1, 2);
+  private final JoystickButton m_JoystickButton3 = new JoystickButton(m_Joystick1, 3);
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
-  // Commands
-  HumanDrive m_TeleopDriveCommand;
-  ShooterAndIntakeCmd m_ShooterAndIntakeCmd;
- 
-  // Controllers
-  // XboxController m_F310;
-  Joystick Driver1;
-  Joystick Operator1;
-  Joystick Operator2;
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+                                                               // driving in open loop
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  private final Telemetry logger = new Telemetry(MaxSpeed);
 
-  // Buttons
-  JoystickButton LockWheels;
-  // JoystickButton OperatorSelectorForward;
-  // JoystickButton OperatorSelectorBackward;
+  private void configureBindings() {
+    drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+        drivetrain.applyRequest(() -> drive.withVelocityX(-m_Joystick1.getRawAxis(1) * MaxSpeed) // Drive forward with
+                                                                                           // negative Y (forward)
+            .withVelocityY(-m_Joystick1.getRawAxis(0) * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-m_Joystick2.getRawAxis(0) * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        ));
 
-  // Other Definitions
-  SendableChooser<Command> AutonChooser;
-  Pigeon2 Pigeon;
+    // m_Joystick1.getRawButton(0).whileTrue(drivetrain.applyRequest(() -> brake));
+   m_JoystickTrigger.whileTrue(drivetrain.applyRequest(() -> brake));
+    m_JoystickButton2.whileTrue(drivetrain
+        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-m_Joystick1.getRawAxis(0), -m_Joystick1.getRawAxis(1)))));
+
+    // reset the field-centric heading on left bumper press
+    m_JoystickButton3.onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+
+    if (Utils.isSimulation()) {
+      drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
+    }
+    drivetrain.registerTelemetry(logger::telemeterize);
+  }
 
   public RobotContainer() {
-    // Controllers
-    Driver1 = new Joystick(Constants.Joystick_Port.Driver1Port); // Driver 1
-    Driver2 = new Joystick(Constants.Joystick_Port.Driver2Port); // Driver 2
-    Operator1 = new Joystick(Constants.Joystick_Port.Operator1Port); // Operator 1
-    Operator2 = new Joystick(Constants.Joystick_Port.Operator2Port); // Operator 2
-
-    // Subsystems
-    m_DriveSubsystem = new DriveSubsystem();
-    m_AutonSubsystem = new AutonSubsystem();
-    m_VisionSubsystem = new VisionSubsystem("limelight-front");
-  
-    // Commands
-    m_TeleopDriveCommand = new HumanDrive(m_DriveSubsystem, Driver1, Driver2);
-    m_ShooterAndIntakeCmd = new ShooterAndIntakeCmd();
-  
-    // Buttons
-    LockWheels = new JoystickButton(Driver2, 3);
-
-    // PidgeonAngle
-    SmartDashboard.putNumber("Current Pigeon Angle", DriveSubsystem.getHeading());
-
-    // m_OperatorInterfaceSubsystem = new OperatorInterfaceSubsystem();
-
-    // Auton Path Chooser
-    AutonChooser = new SendableChooser<Command>();
-
-    AutonChooser.setDefaultOption("No Path", null);
-    
-
-    SmartDashboard.putData("Auton Chooser", AutonChooser);
-
-    configureButtonBindings();
-  }
-
-  public void setTeleopBindings() {
-    m_DriveSubsystem.setDefaultCommand(m_TeleopDriveCommand);
-    LockWheels.whileTrue(new LockWheels(m_DriveSubsystem));
-  }
-
-  public void scheduleTeleopCommands() {
-    m_ShooterAndIntakeCmd.schedule();
-  }
-
-  private void configureButtonBindings() {
+    configureBindings();
   }
 
   public Command getAutonomousCommand() {
-    System.out.println(AutonChooser.getSelected());
-    return AutonChooser.getSelected();
+    return Commands.print("No autonomous command configured");
   }
 }
