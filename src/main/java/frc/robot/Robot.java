9951311a6452f679f996d21101c85c589d3a50b1
Driver 2 <som624686@stu.aasd.k12.wi.us>
2024-01-18 13:37:12 -0600

    [Working] Created a Tuner X Swerve project

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,83 +1,76 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.SwerveModule;
-
 
 public class Robot extends TimedRobot {
-
   private Command m_autonomousCommand;
+
   private RobotContainer m_robotContainer;
-  public static SwerveModule Front_Left;
-  public static SwerveModule Front_Right;
-  public static SwerveModule Back_Left;
-  public static SwerveModule Back_Right;
+
   @Override
   public void robotInit() {
-    setNetworkTablesFlushEnabled(true);
     m_robotContainer = new RobotContainer();
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-    m_robotContainer.m_TeleopDriveCommand.disableLights();
   }
 
   @Override
   public void robotPeriodic() {
-
-    CommandScheduler.getInstance().run();
-
-    SmartDashboard.putBoolean("Enabled?", isEnabled());
-
+    CommandScheduler.getInstance().run(); 
   }
 
   @Override
-  public void disabledInit() {
-    m_robotContainer.m_TeleopDriveCommand.disableLights();
-  }
+  public void disabledInit() {}
 
   @Override
-  public void disabledPeriodic() {
+  public void disabledPeriodic() {}
 
-  }
+  @Override
+  public void disabledExit() {}
 
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
-
   }
 
   @Override
-  public void autonomousPeriodic() {
-  }
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
 
   @Override
   public void teleopInit() {
-
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-
-    m_robotContainer.scheduleTeleopCommands();
-    m_robotContainer.setTeleopBindings();
   }
 
   @Override
-  public void teleopPeriodic() {
-  }
+  public void teleopPeriodic() {}
+
+  @Override
+  public void teleopExit() {}
 
   @Override
   public void testInit() {
-
+    CommandScheduler.getInstance().cancelAll();
   }
 
   @Override
-  public void testPeriodic() {
+  public void testPeriodic() {}
 
-    
-  }
+  @Override
+  public void testExit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
