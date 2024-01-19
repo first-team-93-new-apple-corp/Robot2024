d027a8b0aba9d10069fdbb133008dfc041a9bee9
da_winkz <neonitrogen127@gmail.com>
2024-01-18 20:54:13 -0600

    Changed speeds with peer supervition to make it just right.

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -12,50 +12,66 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
 
-public class ShooterSubsystem  extends SubsystemBase {
+public class ShooterSubsystem extends SubsystemBase {
     static TalonFX ShooterR = new TalonFX(05);
     static TalonFX ShooterL = new TalonFX(11);
     static CANSparkMax IntoShooter = new CANSparkMax(3, MotorType.kBrushless);
-    static double SpeakerShooterSpeed = 0.6;
+    static double SpeakerShooterSpeed = 0.65;
     static double currentspeed;
     final static double AmpShooterSpeed = 0.3;
     final static double KickerSpeed = -1;
-    public static void shootSpeaker(){
+
+    public static void shootSpeaker() {
         System.out.println("shooting speaker");
-            ShooterR.set( -SpeakerShooterSpeed);
-            ShooterL.set( SpeakerShooterSpeed);
-        
+        ShooterR.set(-SpeakerShooterSpeed);
+        ShooterL.set(SpeakerShooterSpeed);
+
     }
-    public static void shootAmp(){
-        ShooterR.set( -AmpShooterSpeed);
-        ShooterL.set( AmpShooterSpeed);
+
+    public static void shootAmp() {
+        ShooterR.set(-AmpShooterSpeed);
+        ShooterL.set(AmpShooterSpeed);
     }
-    public static void shootIntake(){
+
+    public static void shootIntake() {
         IntoShooter.set(KickerSpeed);
     }
-    public static void shootMuzzle(){
-        ShooterR.set( SpeakerShooterSpeed);
-        ShooterL.set( -SpeakerShooterSpeed);
+
+    public static void shootMuzzle() {
+        ShooterR.set(0.15);
+        ShooterL.set(-0.15);
     }
-    public static void shootStop(){
+
+    public static void shootStop() {
         ShooterR.set(0);
         ShooterL.set(0);
     }
-    public static void shootIntakeStop(){
+
+    public static void shootIntakeStop() {
         IntoShooter.set(0);
     }
-    public static void shootPlus(){
-        SpeakerShooterSpeed += 0.05; //+5%speed
+
+    public static void shootPlus() {
+        SpeakerShooterSpeed += 0.05; // +5%speed
         SmartDashboard.putNumber("CurrentSpeed", SpeakerShooterSpeed);
-        
+
     }
-    public static void shootMinus(){
-        SpeakerShooterSpeed -= 0.05; //-5%speed
+
+    public static void shootMinus() {
+        SpeakerShooterSpeed -= 0.05; // -5%speed
         SmartDashboard.putNumber("CurrentSpeed", SpeakerShooterSpeed);
-        
+
     }
-    public static void shootConstants(){
+
+    public static void shootConstants() {
         SmartDashboard.putNumber("Shooter Speed", SpeakerShooterSpeed);
         SmartDashboard.putNumber("ShooterRmp", ShooterR.getVelocity().getValueAsDouble());
+        SmartDashboard.putNumber("Shooter 1 Temp", ShooterL.getDeviceTemp().getValueAsDouble());
+        SmartDashboard.putNumber("Shooter 2 Temp", ShooterR.getDeviceTemp().getValueAsDouble());
+    }
+
+    @Override
+    public void periodic() {
+       
     }
 }
