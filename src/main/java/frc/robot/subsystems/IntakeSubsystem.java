f647b33509a8d5e58f459b6f7f23fbf45eb11b73
LavaWolf911 <Connorw911@gmail.com>
2024-01-22 19:40:19 -0600

    Fixed acidenal addition to merge

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,30 +9,22 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 public class IntakeSubsystem extends SubsystemBase {
     static CANSparkMax NeoIntakeR = new CANSparkMax(2, MotorType.kBrushless);
     static CANSparkMax NeoIntakeL = new CANSparkMax(3, MotorType.kBrushless);
-    final static boolean ifNote = false;
     final static double IntakeShooterSpeed = 0.75;
 
     public static void Intake() {
-        if (ifNote == false){
         NeoIntakeR.set(IntakeShooterSpeed);
         NeoIntakeL.set(-IntakeShooterSpeed);
-        }
     }
 
     public static void IntakePassover() { 
-        if (ifNote == true){
         NeoIntakeR.set(IntakeShooterSpeed);
-        NeoIntakeL.set(-IntakeShooterSpeed);
-        }
     }
 
     public static void IntakeStop() {
-        NeoIntakeFront.set(0);
-        NeoIntakeBack.set(0);
+        NeoIntakeR.set(0);
+        NeoIntakeL.set(0);
     }
 
     public static void IntakeConstants() {
-        // if note is in kicker make ifNote True
-        SmartDashboard.putBoolean("Note intaked", ifNote);
     }
 }
