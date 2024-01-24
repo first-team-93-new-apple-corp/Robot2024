dee97abeeaac0c8317621ede68ceeb66021cd0f6
Driver 2 <som624686@stu.aasd.k12.wi.us>
2024-01-24 07:45:59 -0600

    DO NOT AT ANY POINT IN TIME, LET KATE OR ROOKIES TOUCH THIS STUFF I AM NOT DEALING WITH THIS AGAIN >:(

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,24 +7,24 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 
 public class IntakeSubsystem extends SubsystemBase {
-    static CANSparkMax NeoIntakeFront = new CANSparkMax(2, MotorType.kBrushless);
-    static CANSparkMax NeoIntakeBack = new CANSparkMax(3, MotorType.kBrushless);
+    // static CANSparkMax NeoIntakeFront = new CANSparkMax(2, MotorType.kBrushless);
+    // static CANSparkMax NeoIntakeBack = new CANSparkMax(3, MotorType.kBrushless);
 
     final static double IntakeShooterSpeed = 0.75;
 
     public static void Intake() {
-        NeoIntakeFront.set(IntakeShooterSpeed);
-        NeoIntakeBack.set(-IntakeShooterSpeed);
+        // NeoIntakeFront.set(IntakeShooterSpeed);
+        // NeoIntakeBack.set(-IntakeShooterSpeed);
     }
 
     public static void IntakePassover() {
-        NeoIntakeFront.set(IntakeShooterSpeed);
-        NeoIntakeBack.set(-IntakeShooterSpeed);
+        // NeoIntakeFront.set(IntakeShooterSpeed);
+        // NeoIntakeBack.set(-IntakeShooterSpeed);
     }
 
     public static void IntakeStop() {
-        NeoIntakeFront.set(0);
-        NeoIntakeBack.set(0);
+        // NeoIntakeFront.set(0);
+        // NeoIntakeBack.set(0);
     }
 
     public static void IntakeConstants() {
