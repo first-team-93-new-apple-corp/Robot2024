4186ad89176b1ffa30853f9b882f9aa60c3296b1
Driver 2 <som624686@stu.aasd.k12.wi.us>
2024-01-25 20:54:57 -0600

    [not working] funny auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,33 +4,29 @@
 
 package frc.robot;
 
-import com.choreo.lib.Choreo;
-import com.choreo.lib.ChoreoTrajectory;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.HumanDrive;
 import frc.robot.subsystems.DriveConstants;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import frc.robot.subsystems.Telemetry;
 import frc.robot.subsystems.TunerConstants;
-import frc.robot.subsystems.VisionSubsystem;
 
-public class RobotContainer extends TimedRobot{
+public class RobotContainer extends TimedRobot {
+  private final SwerveDriveSubsystem drivetrain = TunerConstants.DriveTrain; // My drivetrain
+  private final SendableChooser<Command> autoChooser;
   public final double MaxSpeed = DriveConstants.MaxSpeed;
   public final double MaxAngularRate = DriveConstants.MaxAngularRate;
   private final Joystick m_Joystick1 = new Joystick(0);
@@ -39,39 +35,26 @@ public class RobotContainer extends TimedRobot{
   private final JoystickButton m_fieldRelButton = new JoystickButton(m_Joystick1,
       Constants.Thrustmaster.Left_Buttons.Top_Middle);
   private final JoystickButton m_JoystickButton2 = new JoystickButton(m_Joystick1, 2);
-  private final SwerveDriveSubsystem drivetrain = TunerConstants.DriveTrain; // My drivetrain
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
       .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
       .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
                                                                // driving in open loop
-  private ChoreoTrajectory traj;
-  private Field2d m_field = new Field2d();
-
-  private final SwerveRequest.RobotCentric robotDrive = new SwerveRequest.RobotCentric()
-      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-      .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-
-  private HumanDrive m_HumanDrive = new HumanDrive(m_Joystick1, m_Joystick2, drivetrain, drive, robotDrive);
-  private VisionSubsystem m_VisionSubsystem = new VisionSubsystem(drivetrain);
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
   private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
   private final Telemetry logger = new Telemetry(MaxSpeed);
 
   // Configures the bindings to drive / control the swerve drive :)
-  private void configureBindings() {
+  public void configureBindings() {
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
         drivetrain.applyRequest(() -> drive
             .withVelocityX(
-                m_HumanDrive.checkJoystickDeadzone(
-                    -m_Joystick1.getRawAxis(Constants.Thrustmaster.Axis.y))
+                    -m_Joystick1.getRawAxis(Constants.Thrustmaster.Axis.y)
                     * MaxSpeed)
             .withVelocityY(
-                m_HumanDrive.checkJoystickDeadzone(
-                    -m_Joystick1.getRawAxis(Constants.Thrustmaster.Axis.x))
+                    -m_Joystick1.getRawAxis(Constants.Thrustmaster.Axis.x)
                     * MaxSpeed)
             .withRotationalRate(
-                m_HumanDrive.checkJoystickDeadzone(
-                    -m_Joystick2.getRawAxis(Constants.Thrustmaster.Axis.x))
+                    -m_Joystick2.getRawAxis(Constants.Thrustmaster.Axis.x)
                     * MaxAngularRate)));
     // Brake while held
     m_JoystickTrigger.onTrue(drivetrain.applyRequest(() -> brake));
@@ -91,70 +74,15 @@ public class RobotContainer extends TimedRobot{
   }
 
   public RobotContainer() {
-    configureBindings();
-    m_VisionSubsystem.updateValues();
-    traj = Choreo.getTrajectory("TEST.1");
-
-    m_field.getObject("traj").setPoses(
-        traj.getInitialPose(), traj.getFinalPose());
-    m_field.getObject("trajPoses").setPoses(
-        traj.getPoses());
-
-    SmartDashboard.putData(m_field);
-
+    drivetrain.configAuto();
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Chooser", autoChooser);
+    
   }
-
-  public boolean mirrorAuto() {
-    if (Alliance.Blue == DriverStation.getAlliance().get()) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
+  
   public Command getAutonomousCommand() {
-    var thetaController = new PIDController(0.005, 0, 0);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-    // drivetrain.resetOdometry(traj.getInitialPose());
-
-    Command swerveCommand = Choreo.choreoSwerveCommand(
-        traj, // Choreo trajectory from above
-        () -> drivetrain.getPose2D().rotateBy(new Rotation2d(180)), // A function that returns the current field-relative pose of the robot: your
-        // wheel or vision odometry
-        new PIDController(0.005, 0.0, 0.0), // PIDController for field-relative X
-                                            // translation (input: X error in meters,
-                                            // output: m/s).
-        new PIDController(0.005, 0.0, 0.0), // PIDController for field-relative Y
-                                            // translation (input: Y error in meters,
-                                            // output: m/s).
-        thetaController, // PID constants to correct for rotation
-                         // error
-        // (ChassisSpeeds speeds) -> drivetrain.drive( // needs to be robot-relative
-        // speeds.vxMetersPerSecond,
-        // speeds.vyMetersPerSecond,
-        // speeds.omegaRadiansPerSecond),
-        (ChassisSpeeds speeds) -> drivetrain.setControl(drive
-            .withVelocityX(
-                speeds.vxMetersPerSecond)
-            .withVelocityY(
-                speeds.vyMetersPerSecond)
-            .withRotationalRate(
-                speeds.omegaRadiansPerSecond)),
-        // () -> mirrorAuto(),
-        () -> true, // Whether or not to mirror the path based on alliance (this assumes the path is
-                            // created for the blue alliance)
-        drivetrain // The subsystem(s) to require, typically your drive subsystem only
-    );
-    return Commands.sequence(
-        drivetrain.runOnce(() -> drivetrain.seedFieldRelative(traj.getInitialPose())),
-        drivetrain.runOnce(() -> drivetrain.resetOdometry(traj.getInitialPose())),
-        swerveCommand,
-        drivetrain.runOnce(
-            () -> drivetrain.applyRequest(() -> drive.withVelocityX(0).withVelocityY(0).withRotationalRate(0))));
+    drivetrain.configAuto();
+    return autoChooser.getSelected();
   }
 
-  public Command getTeleopCommand() {
-    return m_HumanDrive;
-  }
 }
--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -5,22 +5,64 @@ import java.util.function.Supplier;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class SwerveDriveSubsystem extends SwerveDrivetrain implements Subsystem {
-
+    public final double MaxSpeed = DriveConstants.MaxSpeed;
+    public final double MaxAngularRate = DriveConstants.MaxAngularRate;
+    private final SwerveRequest.RobotCentric robotDrive = new SwerveRequest.RobotCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
     private static final double kSimLoopPeriod = 0.005; // 5 ms
     private Notifier m_simNotifier = null;
     private double m_lastSimTime;
+
+    public void configAuto() {
+
+        AutoBuilder.configureHolonomic(
+                this::getPose, // Robot pose supplier
+                this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your
+                                                 // Constants class
+                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                        4.5, // Max module speed, in m/s
+                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red
+                    // alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                this // Reference to this subsystem to set requirements
+        );
+    }
 
     public SwerveDriveSubsystem(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency,
             SwerveModuleConstants... modules) {
@@ -37,13 +79,14 @@ public class SwerveDriveSubsystem extends SwerveDrivetrain implements Subsystem 
         }
     }
 
-    public Pose2d getPose2D() {
+    public Pose2d getPose() {
         // return new Pose2d(this.getRotation3d().getX(), this.getRotation3d().getY(),
-        //         new Rotation2d(this.getRotation3d().getAngle()));
-        return getState().Pose;
+        // new Rotation2d(this.getRotation3d().getAngle()));
+        return m_cachedState.Pose;
     }
+
     public void setPose2D(Pose2d newPose) {
-        getState().Pose = newPose;
+        m_cachedState.Pose = newPose;
     }
 
     public SwerveDrivePoseEstimator getOdometry() {
@@ -54,8 +97,21 @@ public class SwerveDriveSubsystem extends SwerveDrivetrain implements Subsystem 
         return m_kinematics;
     }
 
-    public void resetOdometry(Pose2d pose) {
+    public void resetPose(Pose2d pose) {
         getState().Pose = pose;
+    }
+
+    public ChassisSpeeds getRobotRelativeSpeeds() {
+        return m_kinematics.toChassisSpeeds(m_cachedState.ModuleStates);
+    }
+
+    public void driveRobotRelative(ChassisSpeeds speeds) {
+        // Before leaving DS write what your current goal so someone else can take over
+        // if needed
+        // applyRequest(() -> )
+        setControl(robotDrive.withVelocityX(speeds.vxMetersPerSecond)
+                .withVelocityY(speeds.vyMetersPerSecond)
+                .withRotationalRate(speeds.omegaRadiansPerSecond));
     }
 
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
