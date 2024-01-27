4989c21b164e65f2501bd6aebd7c9c0e2e67dcd4
Driver 2 <som624686@stu.aasd.k12.wi.us>
2024-01-26 18:19:23 -0600

    [not working] pushed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,11 +15,13 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.subsystems.DriveConstants;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import frc.robot.subsystems.Telemetry;
@@ -32,6 +34,7 @@ public class RobotContainer extends TimedRobot {
   public final double MaxAngularRate = DriveConstants.MaxAngularRate;
   private final Joystick m_Joystick1 = new Joystick(0);
   private final Joystick m_Joystick2 = new Joystick(1);
+  private final boolean Limit = true;
   private final JoystickButton m_JoystickTrigger = new JoystickButton(m_Joystick1, 1);
   private final JoystickButton m_fieldRelButton = new JoystickButton(m_Joystick1,
       Constants.Thrustmaster.Left_Buttons.Top_Middle);
@@ -46,6 +49,60 @@ public class RobotContainer extends TimedRobot {
 
   // Configures the bindings to drive / control the swerve drive :)
   public void configureBindings() {
+
+    // public void RotationPoints(Joystick m_Joystick2) {
+    // POVButton pov0 = new POVButton(m_Joystick2, 0); // front
+    // POVButton pov45 = new POVButton(m_Joystick2, 45); // fr wheel
+    // POVButton pov90 = new POVButton(m_Joystick2, 90); // right
+    // POVButton pov135 = new POVButton(m_Joystick2, 135); // br wheel
+    // POVButton pov180 = new POVButton(m_Joystick2, 180); // back
+    // POVButton pov225 = new POVButton(m_Joystick2, 225);// bl wheel
+    // POVButton pov270 = new POVButton(m_Joystick2, 270);// left
+    // POVButton pov315 = new POVButton(m_Joystick2, 315); // fl wheel
+    // POVButton povCenter = new POVButton(m_Joystick2, -1);
+
+    // // SmartDashboard.putNumber("Limit", Limit);
+    // if (!(m_Joystick2.getRawAxis(0) <= 0.1 && m_Joystick2.getRawAxis(0) >= -0.1)) {
+    //   if (Limit) {
+    //     Limit = false;
+    //     // SmartDashboard.putNumber("Limit", Limit);
+    //     if (pov0.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Front
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else if (pov45.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Location_FR
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else if (pov90.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Right
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else if (pov135.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Location_BR
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else if (pov180.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Back
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else if (pov225.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Location_BL
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else if (pov270.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Left
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else if (pov315.getAsBoolean()) {
+    //       DriveConstants.dCenter = DriveConstants.Location_FL
+    //           .rotateBy(Rotation2d.fromDegrees(-1 * SwerveDriveSubsystem.getHeading()));
+    //     } else {
+    //       DriveConstants.dCenter = new Translation2d(0, 0);
+    //     }
+    //   } else {
+    //     Limit = false;
+    //     if (povCenter.getAsBoolean()) {
+    //       DriveConstants.dCenter = new Translation2d(0, 0);
+    //     }
+    //   }
+    // } else {
+    //   Limit = true;
+    // }
+
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
         drivetrain.applyRequest(() -> drive
             .withVelocityX(
@@ -73,6 +130,7 @@ public class RobotContainer extends TimedRobot {
     }
     drivetrain.registerTelemetry(logger::telemeterize);
   }
+  
 
   public RobotContainer() {
     drivetrain.configAuto();
@@ -89,5 +147,4 @@ public class RobotContainer extends TimedRobot {
     // // Create a path following command using AutoBuilder. This will also trigger event markers.
     // return AutoBuilder.followPath(path);
   }
-
 }
