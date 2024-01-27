e413659030f64685e83a1caad5a97be2e9e5eedd
jotishk <jotishkumarsl@gmail.com>
2024-01-27 12:57:21 -0600

    stuff for autoshoot

--- src/main/java/frc/robot/subsystems/AutoShootSubsystem.java
+++ src/main/java/frc/robot/subsystems/AutoShootSubsystem.java
@@ -1,7 +1,29 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AutoShootSubsystem extends SubsystemBase{
+    NetworkTable networkTable;
+    double tx;
+    double ty;
+    double ta;
+    double tv;
     
+    public AutoShootSubsystem() {
+        networkTable = NetworkTableInstance.getDefault().getTable("Pipeline_Name");
+    }
+    public boolean hasTargets() {
+        return tv ==1;
+    }
+    public void alignSpeaker() {
+        
+    }
+    public void periodic() {
+        tx = networkTable.getValue("tx").getDouble();
+        ty = networkTable.getValue("ty").getDouble();
+        ta = networkTable.getValue("ta").getDouble();
+        tv = networkTable.getValue("tv").getDouble();
+    }
 }
