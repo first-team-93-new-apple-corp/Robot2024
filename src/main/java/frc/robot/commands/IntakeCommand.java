8d4f9b11a62125431450f272aeb931f6c25c5e89
LavaWolf911 <Connorw911@gmail.com>
2024-01-27 09:38:47 -0600

    changed controller port and name

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class IntakeCommand extends Command {
-    XboxController js = new XboxController(4);
+    XboxController opController = new XboxController(2);
     private IntakeCommand m_IntakeCommand;
 
     public void execute() {
