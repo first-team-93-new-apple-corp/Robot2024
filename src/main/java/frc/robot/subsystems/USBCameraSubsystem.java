027426ec93f4af8abaecb8d9016958c8a3ff8521
da_winkz <neonitrogen127@gmail.com>
2024-02-10 09:38:43 -0600

    Got rid of unused imports

--- src/main/java/frc/robot/subsystems/USBCameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/USBCameraSubsystem.java
@@ -2,10 +2,6 @@ package frc.robot.subsystems;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
