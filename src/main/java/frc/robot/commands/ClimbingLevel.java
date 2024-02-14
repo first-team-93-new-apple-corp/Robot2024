9e88f1e4c080e8772c1b60daa4d54f921c48cec7
jotishk <jotishkumarsl@gmail.com>
2024-02-13 20:34:56 -0600

    created branch

--- src/main/java/frc/robot/commands/ClimbingLevel.java
+++ src/main/java/frc/robot/commands/ClimbingLevel.java
@@ -1,41 +1,41 @@
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Robot;
-import frc.robot.subsystems.ElevatorSubsystem;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.Robot;
+// import frc.robot.subsystems.ElevatorSubsystem;
 
-import com.ctre.phoenix6.hardware.*;
-public class ClimbingLevel extends Command {
-    ClimberCommand m_climberCommand;
-    ElevatorCommand m_ElevatorCommand;
-    ElevatorSubsystem m_ElevatorSubsystem;
-    Pigeon2 m_pigeon2;
-    double elevatorSetpoint = -20.0;
-    Robot m_robot;
-    public ClimbingLevel(
-            ClimberCommand m_climberCommand) {
-        // Use addRequirements() here to declare subsystem dependencies.
-        this.m_climberCommand = m_climberCommand;
-        m_ElevatorCommand = new ElevatorCommand();
-        m_robot = new Robot();
-        m_pigeon2 = m_robot.getPigeon();
+// import com.ctre.phoenix6.hardware.*;
+// public class ClimbingLevel extends Command {
+//     ClimberCommand m_climberCommand;
+//     ElevatorCommand m_ElevatorCommand;
+//     ElevatorSubsystem m_ElevatorSubsystem;
+//     Pigeon2 m_pigeon2;
+//     double elevatorSetpoint = -20.0;
+//     Robot m_robot;
+//     public ClimbingLevel(
+//             ClimberCommand m_climberCommand) {
+//         // Use addRequirements() here to declare subsystem dependencies.
+//         this.m_climberCommand = m_climberCommand;
+//         m_ElevatorCommand = new ElevatorCommand();
+//         m_robot = new Robot();
+//         m_pigeon2 = m_robot.getPigeon();
 
-        /*
-         * I don't think that it is needed atm but we could take the time to implement
+//         /*
+//          * I don't think that it is needed atm but we could take the time to implement
 
-         as a like precaution?
+//          as a like precaution?
 
-         looked over the code and elevator already has PIDs in the toSetpoint method in the Subsystem
+//          looked over the code and elevator already has PIDs in the toSetpoint method in the Subsystem
          
-         */
-        if(m_pigeon2.getRoll().getValueAsDouble() > 0.5) {
-            m_climberCommand.changeLeft(-5);
-            m_climberCommand.changeRight(5);
-        } else if(m_pigeon2.getRoll().getValueAsDouble() < -0.5) {
-            m_climberCommand.changeLeft(5);
-            m_climberCommand.changeRight(-5);
-        }
-        //This doesn't have to change since the weight forward/backwards should always be the same
-        m_ElevatorSubsystem.toSetpoint(elevatorSetpoint);
-    }
-}
+//          */
+//         if(m_pigeon2.getRoll().getValueAsDouble() > 0.5) {
+//             m_climberCommand.changeLeft(-5);
+//             m_climberCommand.changeRight(5);
+//         } else if(m_pigeon2.getRoll().getValueAsDouble() < -0.5) {
+//             m_climberCommand.changeLeft(5);
+//             m_climberCommand.changeRight(-5);
+//         }
+//         //This doesn't have to change since the weight forward/backwards should always be the same
+//         m_ElevatorSubsystem.toSetpoint(elevatorSetpoint);
+//     }
+// }
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,51 +1,92 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.commands.ShooterAndIntakeCmd;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Joystick;
 
-public class ClimberSubsystem extends SubsystemBase {
-    public TalonFX climberLeft;
-    public TalonFX climberRight;
-    public XboxController op;
-
-    public ClimberSubsystem(XboxController op) {
-        this.op = op;
-        climberLeft = new TalonFX(Constants.CTRE.RIO.L_Climber, "rio");
-        climberRight = new TalonFX(Constants.CTRE.RIO.R_Climber, "rio");
-        climberLeft.setNeutralMode(NeutralModeValue.Brake);
-        climberRight.setNeutralMode(NeutralModeValue.Brake);
-    }
+public class ClimberSubsystem  extends SubsystemBase{
+    TalonFX climberMotor1 = new TalonFX(5);
+    TalonFX climberMotor2 = new TalonFX(5);
+    PIDController climber1PID = new PIDController(0,0,0);
+    PIDController climber2PID = new PIDController(0,0,0);
+    boolean motor1Hit = false;
+    boolean motor2Hit = false;
+    double startSetpoint = 0;
+    double extendedSetpoint = 1000;
+    double currentToStop = 2;
+    double loweringSpeed = 0.1;
+    ElevatorFeedforward feedforward1 = new ElevatorFeedforward(0,0,0,0); // change values
+    ElevatorFeedforward feedforward2 = new ElevatorFeedforward(0,0,0,0); // change values
     
-    public void leftSpeed(double speed) {
-        climberLeft.set(speed);
+
+    enum climberState {
+        IDLE,RAISED,LOWERING,LOWERED;
     }
-    public void rightSpeed(double speed) {
-        climberRight.set(speed);
+
+    climberState state = climberState.IDLE;
+    
+    public ClimberSubsystem() {
+        climberMotor1.setNeutralMode(NeutralModeValue.Brake);
+        climberMotor1.setNeutralMode(NeutralModeValue.Brake);
+
     }
-    public void zeroLeft() {
-        climberLeft.setPosition(0);
+    public void raiseClimber() {
+        state = climberState.RAISED;
     }
-    public void zeroRight() {
-        climberRight.setPosition(0);
+    public void lowerClimber() {
+        state = climberState.LOWERING;       
     }
-    public double getLeftDraw() {
-        return climberLeft.getSupplyCurrent().getValueAsDouble();
-    }
-    public double getRightDraw() {
-        return climberRight.getSupplyCurrent().getValueAsDouble();
-    }
-    public double leftPosition() {
-        return climberLeft.getPosition().getValueAsDouble();
-    }
-    public double rightPosition() {
-        return climberRight.getPosition().getValueAsDouble();
-    }
+  
+    
+        
+    
     @Override
     public void periodic() {
-
+        SmartDashboard.putNumber("Climber1 Encoder", climberMotor1.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("Climber2 Encoder", climberMotor2.getPosition().getValueAsDouble());
+        SmartDashboard.putString("Climber State", state.toString());
+        if (state.equals(climberState.RAISED)) {
+            climberMotor1.set(climber1PID.calculate(extendedSetpoint));
+            climberMotor2.set(climber1PID.calculate(extendedSetpoint));
+        } else if (state.equals(climberState.IDLE)) {
+            climberMotor1.set(climber1PID.calculate(startSetpoint));
+            climberMotor2.set(climber1PID.calculate(startSetpoint));
+        } else if (state.equals(climberState.LOWERED)) {
+            climberMotor1.set(feedforward1.calculate(climber1PID.calculate(startSetpoint)));
+            climberMotor2.set(feedforward2.calculate(climber1PID.calculate(startSetpoint)));
+        } else if (state.equals(climberState.LOWERING)) { 
+            if (climberMotor1.getSupplyCurrent().getValue() > 2) {
+                motor1Hit = true;
+                climberMotor1.set(0);
+                climberMotor1.setNeutralMode(NeutralModeValue.Brake);
+            }
+            if (climberMotor2.getSupplyCurrent().getValue() > 2) {
+                motor2Hit = true;
+                climberMotor2.set(0);
+                climberMotor2.setNeutralMode(NeutralModeValue.Brake);
+            } 
+            if (!motor1Hit) {
+                climberMotor1.set(loweringSpeed);
+            }
+            if (!motor2Hit) {
+                climberMotor1.set(loweringSpeed);
+            }
+            if (motor1Hit && motor2Hit) {
+                state = climberState.LOWERED;
+            }
+     
+        } 
+        
     }
 }
