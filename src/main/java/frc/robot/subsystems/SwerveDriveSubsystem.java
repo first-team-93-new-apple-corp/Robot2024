ceb0129b2fef3fe345188d84cf4b80157ab53403
Driver 2 <som624686@stu.aasd.k12.wi.us>
2024-03-01 08:15:37 -0600

    [Not working] Auto doesn't work. Tried fixing

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -24,6 +24,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
