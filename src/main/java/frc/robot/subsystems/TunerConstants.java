2df1eef1656fddc90838347d80253e72bedd5dee
Driver 2 <som624686@stu.aasd.k12.wi.us>
2024-03-08 09:48:43 -0600

    [Working] updated tuner constants for Krakens

--- src/main/java/frc/robot/subsystems/TunerConstants.java
+++ src/main/java/frc/robot/subsystems/TunerConstants.java
@@ -90,7 +90,7 @@ public class TunerConstants {
     private static final int kFrontLeftDriveMotorId = 1;
     private static final int kFrontLeftSteerMotorId = 5;
     private static final int kFrontLeftEncoderId = 10;
-    private static final double kFrontLeftEncoderOffset = 0.00146484375;
+    private static final double kFrontLeftEncoderOffset = -0.01123046875;
 
     private static final double kFrontLeftXPosInches = 11.25;
     private static final double kFrontLeftYPosInches = 11.25;
@@ -99,7 +99,7 @@ public class TunerConstants {
     private static final int kFrontRightDriveMotorId = 2;
     private static final int kFrontRightSteerMotorId = 6;
     private static final int kFrontRightEncoderId = 11;
-    private static final double kFrontRightEncoderOffset = -0.20361328125;
+    private static final double kFrontRightEncoderOffset = 0.358642578125;
 
     private static final double kFrontRightXPosInches = 11.25;
     private static final double kFrontRightYPosInches = -11.25;
@@ -108,7 +108,7 @@ public class TunerConstants {
     private static final int kBackLeftDriveMotorId = 4;
     private static final int kBackLeftSteerMotorId = 8;
     private static final int kBackLeftEncoderId = 13;
-    private static final double kBackLeftEncoderOffset = -0.314208984375;
+    private static final double kBackLeftEncoderOffset = 0.29052734375;
 
     private static final double kBackLeftXPosInches = -11.25;
     private static final double kBackLeftYPosInches = 11.25;
@@ -117,7 +117,7 @@ public class TunerConstants {
     private static final int kBackRightDriveMotorId = 3;
     private static final int kBackRightSteerMotorId = 7;
     private static final int kBackRightEncoderId = 12;
-    private static final double kBackRightEncoderOffset = 0.111083984375;
+    private static final double kBackRightEncoderOffset = -0.191162109375;
 
     private static final double kBackRightXPosInches = -11.25;
     private static final double kBackRightYPosInches = -11.25;
