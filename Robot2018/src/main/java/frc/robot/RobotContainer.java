059bc2c62dea2290d77f6083d269dad3c4dd9ead
jsmmr2 <justintsommer@icloud.com>
2024-10-16 15:30:14 -0500

    [Working] started drivetrain

--- Robot2018/src/main/java/frc/robot/RobotContainer.java
+++ Robot2018/src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+  private CommandXboxController m_XboxController = new CommandXboxController(0);
   public RobotContainer() {
     configureBindings();
   }
---
+++ Robot2018/src/main/java/frc/robot/subsystems/Drivetrain/TankDriveSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.Drivetrain;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class TankDriveSubsystem implements Subsystem {
+    private VictorSPX frontLeft;
+    private VictorSPX frontRight;
+    private VictorSPX backLeft;
+    private VictorSPX backRight;
+    
+    public TankDriveSubsystem() {
+        frontLeft = new VictorSPX(0);
+        frontRight = new VictorSPX(1);
+        backLeft = new VictorSPX(2);
+        backRight = new VictorSPX(3);
+    }
+    
+}
