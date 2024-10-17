581afc8625f6ec7cda6b491b271291c856a6af13
jsmmr2 <justintsommer@icloud.com>
2024-10-17 11:04:46 -0500

    [Untested] Basic drivetrain code using xbox controller on port 0

--- Robot2018/src/main/java/frc/robot/RobotContainer.java
+++ Robot2018/src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,23 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drivetrain.TankDriveSubsystem;
 
 public class RobotContainer {
   private CommandXboxController m_XboxController = new CommandXboxController(0);
+
+  private TankDriveSubsystem m_drivetrain = new TankDriveSubsystem();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_drivetrain.setDefaultCommand(
+        m_drivetrain.run(
+            () -> m_drivetrain.drive(m_XboxController.getLeftY(), m_XboxController.getRightX())));
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- Robot2018/src/main/java/frc/robot/subsystems/Drivetrain/TankDriveSubsystem.java
+++ Robot2018/src/main/java/frc/robot/subsystems/Drivetrain/TankDriveSubsystem.java
@@ -1,24 +1,40 @@
 package frc.robot.subsystems.Drivetrain;
 
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class TankDriveSubsystem implements Subsystem {
-    private VictorSPX frontLeft;
-    private VictorSPX frontRight;
-    private VictorSPX backLeft;
-    private VictorSPX backRight;
+    private final VictorSPX frontLeft;
+    private final VictorSPX frontRight;
+    private final VictorSPX backLeft;
+    private final VictorSPX backRight;
+
+    private final DifferentialDrive m_drivetrain;
 
     public TankDriveSubsystem() {
-        frontLeft = new VictorSPX(0);
-        frontRight = new VictorSPX(1);
-        backLeft = new VictorSPX(2);
-        backRight = new VictorSPX(3);
+        frontLeft = new VictorSPX(5);
+        frontRight = new VictorSPX(4);
+        backLeft = new VictorSPX(0);
+        backRight = new VictorSPX(10);
+        
+        //TODO: This needs to be changed if the motors fight each other
+        backLeft.setInverted(false);
+        backRight.setInverted(false);
 
-        // frontLeft.addFollower(backLeft);
+        backLeft.follow(frontLeft);
+        backRight.follow(frontRight);
+
+        m_drivetrain = new DifferentialDrive(
+                (speed) -> frontLeft.set(VictorSPXControlMode.PercentOutput, speed),
+                (speed) -> frontRight.set(VictorSPXControlMode.PercentOutput, speed));
+
     }
-    
+
+    public void drive(double xSpeed, double zRotation) {
+        m_drivetrain.arcadeDrive(xSpeed, zRotation);
+    }
+
 }
