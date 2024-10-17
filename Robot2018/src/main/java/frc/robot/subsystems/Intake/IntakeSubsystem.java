df3ac2c556d73a7427a475e226b2294030bdc95c
jsmmr2 <justintsommer@icloud.com>
2024-10-17 14:46:30 -0500

    [Untested] Added sensors foor intake (I forgor)

--- Robot2018/src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
+++ Robot2018/src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.Intake;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
@@ -7,18 +8,31 @@ public class IntakeSubsystem implements Subsystem {
     private PWMVictorSPX m_leftIntake;
     private PWMVictorSPX m_rightIntake;
 
+    private AnalogInput m_leftSensor;
+    private AnalogInput m_rightSensor;
+
     public IntakeSubsystem() {
         m_leftIntake = new PWMVictorSPX(0);
         m_rightIntake = new PWMVictorSPX(1);
+
+        m_leftSensor = new AnalogInput(0);
+        m_rightSensor = new AnalogInput(1);
     }
+
     public void intake() {
-        m_leftIntake.set(0.5);
-        m_rightIntake.set(0.5);
+        if (!(m_leftSensor.getVoltage() > 1 && m_rightSensor.getVoltage() > 1)) {
+            m_leftIntake.set(0.5);
+            m_rightIntake.set(0.5);
+        } else {
+            stop();
+        }
     }
+
     public void outtake() {
-        m_leftIntake.set(-0.5); 
+        m_leftIntake.set(-0.5);
         m_rightIntake.set(-0.5);
     }
+
     public void stop() {
         m_leftIntake.set(0);
         m_rightIntake.set(0);
