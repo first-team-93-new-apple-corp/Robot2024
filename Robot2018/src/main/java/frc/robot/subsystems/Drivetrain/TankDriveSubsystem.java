0776d1636545b094d084f82afd2f096d8be65b5b
jsmmr2 <justintsommer@icloud.com>
2024-10-17 09:45:30 -0500

    Started diff drive code

--- Robot2018/src/main/java/frc/robot/subsystems/Drivetrain/TankDriveSubsystem.java
+++ Robot2018/src/main/java/frc/robot/subsystems/Drivetrain/TankDriveSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.Drivetrain;
 
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class TankDriveSubsystem implements Subsystem {
@@ -9,12 +11,14 @@ public class TankDriveSubsystem implements Subsystem {
     private VictorSPX frontRight;
     private VictorSPX backLeft;
     private VictorSPX backRight;
-    
+
     public TankDriveSubsystem() {
         frontLeft = new VictorSPX(0);
         frontRight = new VictorSPX(1);
         backLeft = new VictorSPX(2);
         backRight = new VictorSPX(3);
+
+        // frontLeft.addFollower(backLeft);
     }
     
 }
